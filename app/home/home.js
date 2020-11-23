b362ad7bcc142a4c7961c61bd9a4ef9a14aa39e1
MinhasKamal <minhaskamal024@gmail.com>
2020-11-23 18:46:18 +0600

    updated short intro and contact page

--- app/home/home.js
+++ app/home/home.js
@@ -1,1 +1,22 @@
-'use strict';var homeModule = angular.module('homeModule', [    'ngRoute',]);homeModule.config([    '$routeProvider',        function ($routeProvider) {        $routeProvider            .when('/home', {                templateUrl: 'app/home/home.html',                controller: [                '$scope',                                function($scope){                    siteHeaderText.value="Welcome to My Website";                }],            });    }]);
+'use strict';
+
+var homeModule = angular.module('homeModule', [
+    'ngRoute',
+]);
+
+homeModule.config([
+    '$routeProvider',
+    
+    function ($routeProvider) {
+        $routeProvider
+            .when('/home', {
+                templateUrl: 'app/home/home.html',
+                controller: [
+                '$scope',
+                
+                function($scope){
+                    siteHeaderText.value="I want to make the next morning more beautiful for the world.";
+                }],
+            });
+    }
+]);
